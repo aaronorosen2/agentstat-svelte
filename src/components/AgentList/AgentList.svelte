1d947ce5e893bddb608723a119beae81ee5bea46
DebkantaMondal <debkantaroni01@gmail.com>
2023-09-15 13:48:34 +0530

    mobile style fixed

--- src/components/AgentList/AgentList.svelte
+++ src/components/AgentList/AgentList.svelte
@@ -6,51 +6,58 @@
   let agents = [];
   let loading = false;
   let error = false;
-	export async function searchAgents(filter={}){
-	error = false
-	loading = true
-	let list = {results: []}
-	try {
-		 list = await fetchAgents(filter)  
-		 agents = list.results
-		 let urlParamSelectedUsernames = new URLSearchParams(window.location.search).get('selected_usernames') == undefined ? '' : new URLSearchParams(window.location.search).get('selected_usernames')
-		 let usernames = urlParamSelectedUsernames == '' ? [] :urlParamSelectedUsernames.split(',')
-		 agents.forEach((agent,index)=>{
-		  if(usernames.includes(agent.agent_slug)){
-				agents.splice(index,1)
-				agents.unshift(agent)
-
-		  }
-	 	})
-	}catch(err){
-	 console.error(err.message)
-	 error = true
-		  }
-		  loading = false
-		  return list
+  export async function searchAgents(filter = {}) {
+    error = false;
+    loading = true;
+    let list = { results: [] };
+    try {
+      list = await fetchAgents(filter);
+      agents = list.results;
+      let urlParamSelectedUsernames =
+        new URLSearchParams(window.location.search).get("selected_usernames") ==
+        undefined
+          ? ""
+          : new URLSearchParams(window.location.search).get(
+              "selected_usernames"
+            );
+      let usernames =
+        urlParamSelectedUsernames == ""
+          ? []
+          : urlParamSelectedUsernames.split(",");
+      agents.forEach((agent, index) => {
+        if (usernames.includes(agent.agent_slug)) {
+          agents.splice(index, 1);
+          agents.unshift(agent);
+        }
+      });
+    } catch (err) {
+      console.error(err.message);
+      error = true;
+    }
+    loading = false;
+    return list;
   }
 </script>
 
 <div class="mainContainer">
   <div class="container1">
-    <img src="/images/free_concierge.jpg" alt="" />
+    <img src="/images/free_concierge.jpg" alt="top-banner" />
   </div>
   <div class="container2">
     <div class="blog1">
-      <h4 style=" color: rgb(10, 137, 255);;">
-        Ranked Top 1% On Agentstat: Nationwide Statistics
-      </h4>
+      <h4>Ranked Top 1% On Agentstat: Nationwide Statistics</h4>
     </div>
-    <div class="blog2" />
-    <p><span>Sold Listings</span><span>00</span></p>
-    <p><span>Failed Listings</span><span>00</span></p>
-    <p><span>Avg List to Sold Price</span><span>00</span></p>
-    <p><span>Single Family Sold</span><span>00</span></p>
-    <p><span>Condos Sold</span><span>00</span></p>
+    <div class="blog2">
+      <p><span>Sold Listings</span><span>00</span></p>
+      <p><span>Failed Listings</span><span>00</span></p>
+      <p><span>Avg List to Sold Price</span><span>00</span></p>
+      <p><span>Single Family Sold</span><span>00</span></p>
+      <p><span>Condos Sold</span><span>00</span></p>
+    </div>
   </div>
 
   <div class="container3">
-    <div class="formHeading">
+    <div class="form-heading">
       <h4>For my address, text me two hand-selected Top 1% Agents</h4>
     </div>
     <form action="">
@@ -81,4 +88,4 @@
   {/if}
 </div>
 
-<style src="./agent-list.scss"></style>
+<style src="./agent-list.scss"></style>
